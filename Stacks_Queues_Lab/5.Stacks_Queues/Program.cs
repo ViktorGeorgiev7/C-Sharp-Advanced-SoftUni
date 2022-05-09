f3547e1cb8e70c7f53816b83c1fabecbc53eb555
ViktorGeorgiev7 <103097161+ViktorGeorgiev7@users.noreply.github.com>
2022-05-09 17:53:36 +0300

    Added Stacks and Queues lab exercises.

---
+++ Stacks_Queues_Lab/5.Stacks_Queues/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.Stacks_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)//print even numbers from queue
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(arr);
+
+            while (queue.Count > 0)
+            {
+                if (queue.Count == 1)
+                {
+                    Console.WriteLine(queue.Dequeue());
+
+                    
+                }
+                else
+                {
+                    if (queue.Peek() % 2 == 0)
+                        Console.Write(queue.Dequeue() + ", ");
+                    else
+                    {
+                        queue.Dequeue();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Stacks_Queues_Lab/6.Stacks_Queues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.Stacks_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count> 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(queue.Count + " people remaining.");
+        }
+    }
+}
